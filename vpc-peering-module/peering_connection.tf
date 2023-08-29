8dfb82c6c4378a2d761fa439f31526206fe679ff
Arjun Goel <arjungoel1995@gmail.com>
2023-08-29 21:48:11 +0530

    Update peering_connection.tf

--- vpc-peering-module/peering_connection.tf
+++ vpc-peering-module/peering_connection.tf
@@ -1,7 +1,6 @@
 # Requester's side of the connection.
-resource "aws_vpc_peering_connection" "peer" {
+resource "aws_vpc_peering_connection" "owner" {
   provider = aws.owner
-
   vpc_id        = aws_vpc.owner.id
   peer_vpc_id   = aws_vpc.accepter.id
   peer_owner_id = data.aws_caller_identity.peer.account_id
@@ -14,10 +13,10 @@ resource "aws_vpc_peering_connection" "peer" {
 }
 
 # Accepter's side of the connection.
-resource "aws_vpc_peering_connection_accepter" "peer" {
+resource "aws_vpc_peering_connection_accepter" "accepter" {
   provider = aws.accepter
 
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.owner.id
   auto_accept               = true
 
   tags = {
@@ -30,12 +29,12 @@ resource "aws_route" "owner" {
   provider                  = aws.owner
   route_table_id            = aws_route_table.public_rt.id
   destination_cidr_block    = aws_vpc.accepter.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.owner.id
 }
 
 resource "aws_route" "accepter" {
   provider                  = aws.accepter
   route_table_id            = aws_vpc.accepter.default_route_table_id
   destination_cidr_block    = aws_vpc.owner.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.owner.id
 } 
