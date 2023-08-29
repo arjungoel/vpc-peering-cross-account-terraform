b87ceee7999d721c0dd1fcf3eca5b023c9083eb9
Arjun Goel <arjungoel1995@gmail.com>
2023-08-29 20:29:48 +0530

    Update data.tf

--- vpc-peering-module/data.tf
+++ vpc-peering-module/data.tf
@@ -2,6 +2,10 @@ data "aws_caller_identity" "peer" {
   provider = aws.accepter
 }
 
+data "aws_route_tables" "accepter" {
+  provider = aws.accepter
+  vpc_id   = aws_vpc.accepter.id
+}
 
 data "aws_availability_zones" "available" {
   state = "available"
