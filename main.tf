95a59c641de49a4bb20b051d9402bc0f6d20151c
Arjun Goel <arjungoel1995@gmail.com>
2023-08-29 11:03:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.42.0"
+    }
+  }
+}
 
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+  alias      = "owner"
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key2
+  secret_key = var.secret_key2
+  alias      = "accepter"
+}
+
+module "vpc-peering-module" {
+  source                        = "./vpc-peering-module"
+  region                        = var.region
+  access_key                    = var.access_key
+  secret_key                    = var.secret_key
+  access_key2                   = var.access_key2
+  secret_key2                   = var.secret_key2
+  cidr_block                    = var.cidr_block
+  cidr_block2                   = var.cidr_block2
+  default_tags                  = var.default_tags
+  public_subnet_count           = var.public_subnet_count
+  private_subnet_count          = var.private_subnet_count
+}
